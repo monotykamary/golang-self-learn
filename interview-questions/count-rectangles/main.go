2096c7c2abeb795227bed629b5eab28e771c16f4
Tom Nguyen <tom81094@gmail.com>
2022-06-21 21:23:25 +0700

    docs: update documentation style to block comments for spacing

--- interview-questions/count-rectangles/main.go
+++ interview-questions/count-rectangles/main.go
@@ -7,18 +7,26 @@ import "fmt"
 func countRectangles(rectangles [][]int) int {
 	count := 0
 
-	// Range over rectangles to get its rows and index
+	/*
+		Range over rectangles to get its rows and index
+	*/
 	for i, row := range rectangles {
 
-		// Range over values to get its values and index
+		/*
+			Range over values to get its rows and index
+		*/
 		for j, value := range row {
-			// Determine if the adjacent numbers are 0; if they are, add to count; else we'll continue traversing
-			// Check if the current number is 0
+			/*
+				Determine if the adjacent numbers are 0; if they are, add to count; else we'll continue traversing.
+				Check if the current number is 0.
+			*/
 			if value == 0 {
 				continue
 			}
 
-			// Check if we're not at the start of the row and check if the number on top of us is 1
+			/*
+				Check if we're not at the start of the row and check if the number on top of us is 1
+			*/
 			if i > 0 {
 				upValue := rectangles[i-1][j]
 				if upValue == 1 {
@@ -26,7 +34,7 @@ func countRectangles(rectangles [][]int) int {
 				}
 			}
 
-			// Check if we're not at the start of the row and check if the number on left of us is 1
+			// Check if we're not at the start of the column and check if the number on left of us is 1
 			if j > 0 {
 				leftValue := rectangles[i][j-1]
 				if leftValue == 1 {
